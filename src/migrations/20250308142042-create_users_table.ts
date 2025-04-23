39b165289fb914cee2f33b680b359445cbbc510e
Aingareng <ngarengai@gmail.com>
2025-04-23 21:24:30 +0800

    fix: update reference model name from 'roles' to 'RoleModel' in users table migration

--- src/migrations/20250308142042-create_users_table.ts
+++ src/migrations/20250308142042-create_users_table.ts
@@ -12,7 +12,7 @@ module.exports = {
         allowNull: false,
         type: DataTypes.INTEGER,
         references: {
-          model: "roles",
+          model: "RoleModel",
           key: "id",
         },
       },
